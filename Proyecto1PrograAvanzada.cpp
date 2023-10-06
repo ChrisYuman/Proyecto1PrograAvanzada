153bf0119d9112cc3482c838dede04a9f6472435
Pablo <andresbocel0@gmail.com>
2023-10-06 17:23:22 -0600

    ArregloRepsiguiente

--- Proyecto1PrograAvanzada.cpp
+++ Proyecto1PrograAvanzada.cpp
@@ -12,184 +12,188 @@ using namespace std;
 
 int main()
 {
+    try {
 
-    cout << "Proyecto 1 programacion avanzada\n" << endl; 
-    cout << "--------------------Bienvenido, en esta aplicacion podra almacenar sus CDs para poder reprdocucirlos--------------------" << endl; 
-    ReproductorCD reproductor;
-    Agregarcancion agregar; 
-    VerCola vercola;
-    ReproduciActual reproducir; 
-    ReproducirSiguiente siguiente; 
-    Ordenar ordenar; 
+            cout << "Proyecto 1 programacion avanzada\n" << endl;
+            cout << "--------------------Bienvenido, en esta aplicacion podra almacenar sus CDs para poder reprdocucirlos--------------------" << endl;
+            ReproductorCD reproductor;
+            Agregarcancion agregar;
+            VerCola vercola;
+            ReproduciActual reproducir;
+            ReproducirSiguiente siguiente;
+            Ordenar ordenar;
 
-    menu1:
-    cout << "Menu principal" << endl; 
-    cout << "Escoga una de las opciones que se presentan a continuacion" << endl; 
-    cout << "1. Cargar respaldos" << endl;
-    cout << "2. Reproductor de canciones" << endl;
-    cout << "3. Salir del programa" << endl;
-    int opcion;
-    cin >> opcion; 
-    ReproductorCD* reproductorcr = &reproductor;
-    Agregarcancion* agregar1 = &agregar;
-    string carpeta;
+        menu1:
+            cout << "Menu principal" << endl;
+            cout << "Escoga una de las opciones que se presentan a continuacion" << endl;
+            cout << "1. Cargar respaldos" << endl;
+            cout << "2. Reproductor de canciones" << endl;
+            cout << "3. Salir del programa" << endl;
+            int opcion;
+            cin >> opcion;
+            ReproductorCD* reproductorcr = &reproductor;
+            Agregarcancion* agregar1 = &agregar;
+            string carpeta;
 
-    switch (opcion) 
-    {
-        case 1: 
-            system("cls"); //limpiar la pantalla 
-            cout << "--------------------Aqui podra seleccionar los CDs que desee para poder almacenarlos en el programa---------------------\n" << endl;
-            
-            // Solicitar al usuario la ruta de la carpeta de respaldos
-            
-            cout << "Ingrese la ruta de la carpeta de respaldos: ";
-            cin >> carpeta;
+            switch (opcion)
+            {
+            case 1:
+                system("cls"); //limpiar la pantalla 
+                cout << "--------------------Aqui podra seleccionar los CDs que desee para poder almacenarlos en el programa---------------------\n" << endl;
 
-            // Cargar los respaldos desde la carpeta
-            reproductorcr->cargarRespaldosDesdeCarpeta(carpeta);
+                // Solicitar al usuario la ruta de la carpeta de respaldos
 
-            //Imprimir los discos cargados
-            reproductorcr->imprimirDiscos();
+                cout << "Ingrese la ruta de la carpeta de respaldos: ";
+                cin >> carpeta;
 
-            cout << "Desea regresar al menu principal? s=Si, n= No" << endl;
-            char elegir;
-            cin >> elegir;
-            if (elegir == 's') { system("cls");  goto menu1; }
-            else { system("cls"); goto menu2; }
-            
-            break; 
+                // Cargar los respaldos desde la carpeta
+                reproductorcr->cargarRespaldosDesdeCarpeta(carpeta);
 
-                        case 2:
-                        menu2:
-                        system("cls"); 
-                        cout << "Que desea hacer con sus canciones?" << endl;
-                        cout << "1. Agregar cancion" << endl;
-                        cout << "2. Ver cola de reproduccion" << endl;
-                        cout << "3. Reproducir actual" << endl;
-                        cout << "4. Reproducir siguiente" << endl;
-                        cout << "5. Ordenar" << endl;
-                        cout << "6. Regresar al menu principal" << endl;
-                        int opcion2;
-                        cin >> opcion2;
-                        switch (opcion2){
-                            case 1: 
-                                ag:
-                                cout << "Agregar cancion\n" << endl;
-                                Agregarcancion::ejecutarAgregarcancion(&reproductor);
-                               
-                                cout << "su canción se ha agregado" << endl; 
-                                cout << "Desea agregar otra cancion? s=Si, n= No" << endl;
-                                char elegir1;
-                                cin >> elegir1;
-                                if (elegir1 == 's') { system("cls");  goto ag; }
-                                else { goto menu2; }
-                                break; 
-                            case 2:
-                            verc:
-                                system("cls");
-                                cout << "Ver cola de reproduccion\n" << endl;
-                                vercola.mostrarColaDeReproduccion(&reproductor);
+                //Imprimir los discos cargados
+                reproductorcr->imprimirDiscos();
 
-                                cout << "Por favor, regrese al menu con la letra 's'" << endl;
-                                char elegir2;
-                                cin >> elegir2;
-                                if (elegir2 == 's') {  goto menu2; }
-                                else { goto verc; }
-                                break;
-                            case 3:
-                            repa:
-                                system("cls");
-                                cout << "Reproducir actual\n" << endl;
-                                reproducir.mostrarPrimeraCancionEnCola(&reproductor);
-                                
-                                cout << "Por favor, regrese al menu con la letra 's'" << endl;
-                                char elegir3;
-                                cin >> elegir3;
-                                if (elegir3 == 's') {  goto menu2; }
-                                else { goto repa; }
-                                break;
-                            case 4:
-                            reps:
-                                system("cls");
-                                cout << "Reproducir siguiente\n" << endl;
-                                siguiente.reproducirSiguiente(&reproductor);
+                cout << "Desea regresar al menu principal? s=Si, n= No" << endl;
+                char elegir;
+                cin >> elegir;
+                if (elegir == 's') { system("cls");  goto menu1; }
+                else { system("cls"); goto menu2; }
 
-                                cout << "Por favor, regrese al menu con la letra 's'" << endl;
-                                char elegir4;
-                                cin >> elegir4;
-                                if (elegir4 == 's') { goto menu2; }
-                                else { goto reps; }
-                                break;
-                            case 5:
-                                system("cls");
-                                cout << "Ordenar\n" << endl;
-                                cout << "Como desea ordenar la cola?" << endl; 
-                                cout << "1.Nombre del artista ascendente" << endl;
-                                cout << "2.Nombre del artista descendente" << endl;
-                                cout << "3.Nombre de cancion ascendente" << endl;
-                                cout << "4.Nombre de cancion descendente" << endl;
-                                cout << "5.Duracion de cancion ascendente" << endl;
-                                cout << "6.Duracion de cancion descendente" << endl;
-                                cout << "7.Regresar al menu" << endl;
-                                int op; 
-                                cin >> op; 
-                                switch (op)
-                                {
-                                case 1: 
-                                    ordenar.ordenarPorNombreArtistaAscendente(&reproductor);
-                                    goto menu2;
-                                    break; 
-                                case 2:
-                                    ordenar.ordenarPorNombreArtistaDescendente(&reproductor);
-                                    goto menu2;
-                                        break; 
-                                case 3: 
-                                    ordenar.ordenarPorNombreCancionAscendente(&reproductor);
-                                    goto menu2;
-                                    break; 
-                                case 4: 
-                                    ordenar.ordenarPorNombreCancionDescendente(&reproductor);
-                                    goto menu2;
-                                    break; 
-                                case 5: 
-                                    ordenar.ordenarPorDuracionAscendente(&reproductor);
-                                    goto menu2;
-                                    break; 
-                                case 6: 
-                                    ordenar.ordenarPorDuracionDescendente(&reproductor);
-                                    goto menu2;
-                                    break; 
-                                case 7: 
+                break;
 
-                                    goto menu2;
-                                    break; 
-                                default:
-                                    goto menu2;
-                                    break;
-                                }
+            case 2:
+            menu2:
+                system("cls");
+                cout << "Que desea hacer con sus canciones?" << endl;
+                cout << "1. Agregar cancion" << endl;
+                cout << "2. Ver cola de reproduccion" << endl;
+                cout << "3. Reproducir actual" << endl;
+                cout << "4. Reproducir siguiente" << endl;
+                cout << "5. Ordenar" << endl;
+                cout << "6. Regresar al menu principal" << endl;
+                int opcion2;
+                cin >> opcion2;
+                switch (opcion2) {
+                case 1:
+                ag:
+                    cout << "Agregar cancion\n" << endl;
+                    Agregarcancion::ejecutarAgregarcancion(&reproductor);
 
-                                break;
-                            case 6:
-                                system("cls");
-                                goto menu1;
-                                break;
+                    cout << "su canción se ha agregado" << endl;
+                    cout << "Desea agregar otra cancion? s=Si, n= No" << endl;
+                    char elegir1;
+                    cin >> elegir1;
+                    if (elegir1 == 's') { system("cls");  goto ag; }
+                    else { goto menu2; }
+                    break;
+                case 2:
+                verc:
+                    system("cls");
+                    cout << "Ver cola de reproduccion\n" << endl;
+                    vercola.mostrarColaDeReproduccion(&reproductor);
 
-                        default:
-                            goto menu2;
-                            break;
-                        }
+                    cout << "Por favor, regrese al menu con la letra 's'" << endl;
+                    char elegir2;
+                    cin >> elegir2;
+                    if (elegir2 == 's') { goto menu2; }
+                    else { goto verc; }
+                    break;
+                case 3:
+                repa:
+                    system("cls");
+                    cout << "Reproducir actual\n" << endl;
+                    reproducir.mostrarPrimeraCancionEnCola(&reproductor);
+
+                    cout << "Por favor, regrese al menu con la letra 's'" << endl;
+                    char elegir3;
+                    cin >> elegir3;
+                    if (elegir3 == 's') { goto menu2; }
+                    else { goto repa; }
+                    break;
+                case 4:
+                reps:
+                    system("cls");
+                    cout << "Reproducir siguiente\n" << endl;
+                    siguiente.reproducirSiguiente(&reproductor);
+
+                    cout << "Por favor, regrese al menu con la letra 's'" << endl;
+                    char elegir4;
+                    cin >> elegir4;
+                    if (elegir4 == 's') { goto menu2; }
+                    else { goto reps; }
+                    break;
+                case 5:
+                    system("cls");
+                    cout << "Ordenar\n" << endl;
+                    cout << "Como desea ordenar la cola?" << endl;
+                    cout << "1.Nombre del artista ascendente" << endl;
+                    cout << "2.Nombre del artista descendente" << endl;
+                    cout << "3.Nombre de cancion ascendente" << endl;
+                    cout << "4.Nombre de cancion descendente" << endl;
+                    cout << "5.Duracion de cancion ascendente" << endl;
+                    cout << "6.Duracion de cancion descendente" << endl;
+                    cout << "7.Regresar al menu" << endl;
+                    int op;
+                    cin >> op;
+                    switch (op)
+                    {
+                    case 1:
+                        ordenar.ordenarPorNombreArtistaAscendente(&reproductor);
+                        goto menu2;
                         break;
-        case 3:
-            exit(-1);
-            break;
-        default:
-            system("cls");
-            goto menu1;
-            break;
+                    case 2:
+                        ordenar.ordenarPorNombreArtistaDescendente(&reproductor);
+                        goto menu2;
+                        break;
+                    case 3:
+                        ordenar.ordenarPorNombreCancionAscendente(&reproductor);
+                        goto menu2;
+                        break;
+                    case 4:
+                        ordenar.ordenarPorNombreCancionDescendente(&reproductor);
+                        goto menu2;
+                        break;
+                    case 5:
+                        ordenar.ordenarPorDuracionAscendente(&reproductor);
+                        goto menu2;
+                        break;
+                    case 6:
+                        ordenar.ordenarPorDuracionDescendente(&reproductor);
+                        goto menu2;
+                        break;
+                    case 7:
+
+                        goto menu2;
+                        break;
+                    default:
+                        goto menu2;
+                        break;
+                    }
+
+                    break;
+                case 6:
+                    system("cls");
+                    goto menu1;
+                    break;
+
+                default:
+                    goto menu2;
+                    break;
+                }
+                break;
+            case 3:
+                exit(-1);
+                break;
+            default:
+                exit(-1);
+                goto menu1;
+                break;
+            }
+
+
     }
-    
-
-
+    catch (int ex) {
+        cout << "Error en el programa";
+    }
 }
 
 
--- ReproducirSiguiente.cpp
+++ ReproducirSiguiente.cpp
@@ -8,23 +8,33 @@ ReproducirSiguiente::ReproducirSiguiente() {
 void ReproducirSiguiente::reproducirSiguiente(ReproductorCD* reproductor) {
     std::vector<Cancion> colaDeReproduccion = reproductor->getColaDeReproduccion(); // Copia la cola
 
-    if (colaDeReproduccion.size() >= 2) { 
-        Cancion cancionSiguiente = colaDeReproduccion[1];
+    if (colaDeReproduccion.size() >= 2) {
+        // Obtener la primera canción en la cola
+        Cancion primeraCancion = colaDeReproduccion.front();
 
         // Eliminar la primera canción de la cola
         colaDeReproduccion.erase(colaDeReproduccion.begin());
 
-        // Insertar la canción al final de la cola de reproducción
-        colaDeReproduccion.push_back(cancionSiguiente);
+        // Obtener la segunda canción en la cola
+        Cancion segundaCancion = colaDeReproduccion.front();
 
-        // Asignar la copia actualizada de la cola de reproducción al objeto original
+        // Eliminar la segunda canción de la cola
+        colaDeReproduccion.erase(colaDeReproduccion.begin());
+
+        // Insertar la primera canción al final de la cola de reproducción
+        colaDeReproduccion.push_back(primeraCancion);
+
+        // Insertar la segunda canción al inicio de la cola de reproducción
+        colaDeReproduccion.insert(colaDeReproduccion.begin(), segundaCancion);
+
+        // Actualizar la cola de reproducción en el objeto reproductor
         reproductor->setColaDeReproduccion(colaDeReproduccion);
 
         // Mostrar la información de la nueva canción en reproducción
         std::cout << "Reproduciendo Siguiente:" << std::endl;
-        std::cout << "Nombre: " << cancionSiguiente.getNombre() << std::endl;
-        std::cout << "Artista: " << cancionSiguiente.getArtista() << std::endl;
-        std::cout << "Duracion: " << cancionSiguiente.getDuracion() << std::endl;
+        std::cout << "Nombre: " << segundaCancion.getNombre() << std::endl;
+        std::cout << "Artista: " << segundaCancion.getArtista() << std::endl;
+        std::cout << "Duración: " << segundaCancion.getDuracion() << std::endl;
     }
     else {
         std::cout << "No hay suficientes canciones en la cola para reproducir la siguiente." << std::endl;
