08b230fbd8823953ed3ff910b39528b00b74e0aa
Christopher Yuman <chrisyumanv@gmail.com>
2023-10-07 18:39:16 -0600

    Error archivo vacio y linea duplicada hecho

--- ReproductorCD.cpp
+++ ReproductorCD.cpp
@@ -4,11 +4,14 @@
 #include <vector>
 #include <string>
 #include <windows.h>
+#include <unordered_set> // Para detectar líneas duplicadas
 
 ReproductorCD::ReproductorCD() {}
+
 const std::vector<CD>& ReproductorCD::getListaCds() const {
     return listaCds;
 }
+
 void ReproductorCD::cargarRespaldosDesdeCarpeta(const std::string& carpeta) {
     // Limpia la lista de CDs existente si se selecciona esta opción nuevamente
     listaCds.clear();
@@ -21,7 +24,7 @@ void ReproductorCD::cargarRespaldosDesdeCarpeta(const std::string& carpeta) {
     LPCWSTR patronBusqueda = L"/*.txt";
 
     // Combina la carpeta y el patrón de búsqueda en una cadena LPCWSTR
-    std::wstring carpetaConPatron = carpetaW + L"\\*.txt";  
+    std::wstring carpetaConPatron = carpetaW + L"\\*.txt";
     LPCWSTR carpetaConPatronLPCWSTR = carpetaConPatron.c_str();
 
     // Utiliza la biblioteca <windows.h> para listar archivos en el directorio
@@ -40,54 +43,66 @@ void ReproductorCD::cargarRespaldosDesdeCarpeta(const std::string& carpeta) {
     do {
         std::wstring nombreArchivoW = findFileData.cFileName;
         std::string nombreArchivo(nombreArchivoW.begin(), nombreArchivoW.end());
-        std::string rutaCompleta = carpeta + "\\" + nombreArchivo;  
-        cargarCdDesdeArchivo(rutaCompleta);
+        std::string rutaCompleta = carpeta + "\\" + nombreArchivo;
+
+        // Abre el archivo .txt para lectura
+        std::ifstream archivo(rutaCompleta);
+        if (!archivo.is_open()) {
+            std::cerr << "Error al abrir el archivo: " << nombreArchivo << std::endl;
+            continue; // Continuar con el siguiente archivo
+        }
+
+        std::string nombreCd = nombreArchivo;
+        CD cd(nombreCd);
+        bool cdVacio = true; // Bandera para verificar si el CD está vacío
+
+        std::string linea;
+        std::unordered_set<std::string> lineasDuplicadas; // Para detectar líneas duplicadas
+
+        while (std::getline(archivo, linea)) {
+            // Verifica si la línea está duplicada
+            if (lineasDuplicadas.find(linea) != lineasDuplicadas.end()) {
+                std::cerr << "Error: Línea duplicada en el archivo: " << nombreArchivo << std::endl;
+                continue; // Continuar con la siguiente línea
+            }
+
+            // Agrega la línea al conjunto de líneas para verificar duplicados
+            lineasDuplicadas.insert(linea);
+
+            // Divide la línea en nombre, artista y duración utilizando '||' como separador
+            std::string delimiter = "||";
+            size_t pos = 0;
+            std::string token;
+            std::vector<std::string> tokens;  // Declaración del vector
+
+            while ((pos = linea.find(delimiter)) != std::string::npos) {
+                token = linea.substr(0, pos);
+                tokens.push_back(token);
+                linea.erase(0, pos + delimiter.length());
+            }
+            tokens.push_back(linea);
+
+            if (tokens.size() == 3) {
+                Cancion cancion(tokens[0], tokens[1], tokens[2]);
+                cd.agregarCancion(cancion);
+                cdVacio = false; // El CD no está vacío
+            }
+            else {
+                std::cerr << "Formato incorrecto en una línea del archivo: " << nombreArchivo << std::endl;
+            }
+        }
+
+        archivo.close();
+
+        if (!cdVacio) {
+            listaCds.push_back(cd); // Agrega el CD a la lista si no está vacío
+        }
+        else {
+            std::cerr << "Advertencia: El CD en el archivo " << nombreArchivo << " está vacío." << std::endl;
+        }
     } while (FindNextFile(hFind, &findFileData) != 0);
 
     FindClose(hFind);
-}
-
-void ReproductorCD::cargarCdDesdeArchivo(const std::string& nombreArchivo) {
-    // Abre el archivo .txt para lectura
-    std::ifstream archivo(nombreArchivo);
-    if (!archivo.is_open()) {
-        std::cerr << "Error al abrir el archivo: " << nombreArchivo << std::endl;
-        return;
-    }
-
-    std::string nombreCd = nombreArchivo; 
-
-    CD cd(nombreCd); 
-
-    std::string linea;
-    while (std::getline(archivo, linea)) {
-        // Divide la línea en nombre, artista y duración utilizando '||' como separador
-        std::string delimiter = "||";
-        size_t pos = 0;
-        std::string token;
-        std::vector<std::string> tokens;  // Declaración del vector
-
-        while ((pos = linea.find(delimiter)) != std::string::npos) {
-            token = linea.substr(0, pos);
-            tokens.push_back(token);
-            linea.erase(0, pos + delimiter.length());
-        }
-        tokens.push_back(linea);  
-
-        if (tokens.size() == 3) {
-            Cancion cancion(tokens[0], tokens[1], tokens[2]);
-            cd.agregarCancion(cancion);
-        }
-        else {
-            std::cerr << "Formato incorrecto en una línea del archivo: " << nombreArchivo << std::endl;
-        }
-    }
-
-    archivo.close();
-
-    if (cd.getCantidadCanciones() > 0) {
-        listaCds.push_back(cd); // Agrega el CD a la lista si tiene canciones válidas
-    }
 }
 
 void ReproductorCD::imprimirDiscos() const {
